2798cd9aafa954300de850b5c9339f9cbd1d7578
Adrian Mayora <adrianmayora3@gmail.com>
2023-11-28 11:57:42 -0400

    grafica de bitcoin base

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-datos = yf.Ticker ("BTC-USD").history(period="max")
-precio = datos["Close"]
-volumen = datos["Volume"]
+def obtener_datos(ticker):
+    return yf.Ticker(ticker).history(period="max")
 
-plt.figure (figsize=(12,6))
-plt.plot (precio)
-plt.show ()
+def graficar_precio_historico(datos):
+    plt.plot(datos["Close"], label="Precio Histórico")
+
+def graficar_ultimo_precio(datos):
+    ultimo_precio = datos["Close"].iloc[-1]
+    plt.scatter(datos.index[-1], ultimo_precio, color='red', label=f"Último Precio: {ultimo_precio:.2f}")
+
+def configurar_grafica():
+    plt.title("Precio Histórico de Bitcoin")
+    plt.xlabel("Fecha")
+    plt.ylabel("Precio (USD)")
+    plt.legend(loc="upper left")  # Mover la leyenda a la parte superior izquierda
+    plt.show()
+
+# Obtener datos históricos de BTC-USD
+datos_btc = obtener_datos("BTC-USD")
+
+# Crear la gráfica
+plt.figure(figsize=(12, 6))
+
+# Graficar los precios históricos
+graficar_precio_historico(datos_btc)
+
+# Obtener y graficar el último precio
+graficar_ultimo_precio(datos_btc)
+
+# Configurar etiquetas y leyenda
+configurar_grafica()
